e612a786a3c7d8beb7b927f85d8e9e9743e8b91c
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-09-27 16:22:47 +0200

    no steps forward

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let initialBudget = 1000;
+let h1 = document.querySelector("h1");
+h1.innerHTML = initialBudget;
+
+let costs = [100, 200, 300, 400];
+let container = document.getElementById("container");
+
+for (let i = 0; i < costs.length; i++) {
+  container.innerHTML += `<button onclick="subtract(${costs[i]})"> EUR ${costs[i]} </button>`;
+}
