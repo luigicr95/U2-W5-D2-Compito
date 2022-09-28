8290eb7bb19eb736c31582e757a456a3384081e1
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-09-28 14:06:35 +0200

    subtract added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 let initialBudget = 1000;
-let h1 = document.querySelector("h1");
-h1.innerHTML = initialBudget;
+let displayBudget = document.querySelector("h1");
+//let remainingBudget = initialBudget;
+displayBudget.innerHTML = initialBudget;
 
 let costs = [100, 200, 300, 400];
 let container = document.getElementById("container");
 
 for (let i = 0; i < costs.length; i++) {
-  container.innerHTML += `<button onclick="subtract(${costs[i]})"> EUR ${costs[i]} </button>`;
+  container.innerHTML += `<button onclick="subtract(${initialBudget}, ${costs[i]})"> EUR ${costs[i]} </button>`;
 }
 
-//Non sono riuscito a smuovermi dall'impostazione iniziale fornitami
+function subtract(Number1, Number2) {
+  let remainingBudget = (Number1 -= Number2);
+  displayBudget.innerHTML = remainingBudget;
+}
